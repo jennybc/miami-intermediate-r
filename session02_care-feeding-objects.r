430ae4e4a4ad2a06e5d3a1fb3b519549b7b116f3
jennybc <jenny@stat.ubc.ca>
2014-01-27 14:27:57 -0500

    Explored subset and with and vectors, and more

--- session02_care-feeding-objects.r
+++ session02_care-feeding-objects.r
@@ -40,3 +40,60 @@ subset(gDat, subset = country %in% c("Japan", "Belgium"))
 ## get data for which life expectancy is less than 32 years
 ## assign it to an object
 ## how many rows? how many observations per continent?
+
+shortLife <- subset(gDat, subset = lifeExp < 32)
+str(shortLife)
+shortLife
+table(shortLife$continent)
+
+plot(lifeExp ~ year, gDat, subset = country == "Cambodia")
+lm(lifeExp ~ year, gDat, subset = country == "Cambodia")
+
+mean(lifeExp, gDat)
+mean(gDat$lifeExp)
+with(gDat, mean(lifeExp))
+
+with(subset(gDat, country == "Cambodia"),
+     cor(lifeExp, gdpPercap))
+
+x <- 3 * 4
+x
+is.vector(x)
+length(x)
+x[1]
+x[2]
+x[0]
+x[5] <- 10
+x
+
+x <- 1:4
+x
+x^2
+
+round(rnorm(n, mean = c(0, 100, 10000)), 2)
+
+(y <- 1:3)
+(z <- 3:7)
+y + z
+
+(y <- 1:5)
+(z <- 1:10)
+y + z
+
+x <- c(1, 4, 9)
+x <- c("cabbage", pi, 0.3, TRUE)
+str(x)
+
+x <- list("cabbage", pi, 0.3, TRUE)
+str(x)
+x
+
+x <- -3:3
+str(x)
+y <- x < 0
+y
+x
+x <- c("jenny", "bernhard")
+str(x)
+x
+gDat$country
